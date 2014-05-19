79d7f72bc35962d0f724425f57ab7f4f7e8dfb70
TheMrcool212 <themrcool212.android@gmail.com>
2014-05-19 22:12:21 +0800

    Update changelog link
    Conflicts:
    	src/com/paranoid/paranoidota/MainActivity.java

--- src/com/paranoid/paranoidota/MainActivity.java
+++ src/com/paranoid/paranoidota/MainActivity.java
@@ -53,7 +53,7 @@ import com.paranoid.paranoidota.widget.Card;
 
 public class MainActivity extends Activity implements UpdaterListener, DownloadCallback, OnNavigationListener {
 
-    private static final String CHANGELOG = "https://plus.google.com/app/basic/+ParanoidAndroidCorner/posts";
+    private static final String CHANGELOG = "https://plus.google.com/app/basic/+AOSPALorg/posts";
     private static final String STATE = "STATE";
 
     public static final int STATE_UPDATES = 0;
